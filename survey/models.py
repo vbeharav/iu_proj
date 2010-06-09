573dfdd2a68db051024aece4f8ae1a28ac7f26fe
Vamshi Behara <vamshi@plato.(none)>
2010-06-08 21:28:24 -0400

    Refs: Updated models need to be discussed

--- survey/models.py
+++ survey/models.py
@@ -1,33 +1,43 @@
 from django.db import models
 
 # Create your models here.
-class Choices(models.Model):
-    #qforeign = ManyToManyField('Questions',related_name="member choice",db_table="choices_to_questions")
+
+class Questions(models.Model):
+    def __unicode__(self):
+        return self.content
+    mforeign = models.ManyToManyField('Modules',related_name="member question",db_table="questions_to_modules")
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-class Questions(models.Model):
-    #mforeign = models.ManyToManyField('SModules',related_name="member question",db_table="questions_to_modules")
-    choices = models.TextField(blank=True,null=True)
+    
+class Choices(models.Model):
+    def __unicode__(self):
+        return self.content
+    #qforeign = models.ManyToManyField('Questions',related_name="member choice",db_table="choices_to_questions")
     content = models.TextField()
+    quest = models.ForeignKey(Questions)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Modules(models.Model):
+    def __unicode__(self):
+        return self.name
     #sforeign = models.ManyToManyField('Surveys',related_name="member module",db_table="modules_to_surveys")
-    questions = models.TextField()
+    
+    questions = models.TextField(blank=True)
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Surveys(models.Model):
+    def __unicode__(self):
+        return self.name
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True,null=True)
-    modules = models.TextField()
+    modules = models.TextField(blank=True)
     branches = models.TextField(blank=True,null=True)
-
+    
 class Branches(models.Model):
     #brach_to_survey = models.ManyToManyField('Surveys',related_name="member branch",db_table="branches_to_surveys")
     msource = models.PositiveIntegerField()
